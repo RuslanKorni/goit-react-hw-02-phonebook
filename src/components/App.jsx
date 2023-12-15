aa189c965182d9c2951272bcef0d9dc53ba952c4
RuslanKorni <ruslankornijcenko4@gmail.com>
2023-12-15 13:28:19 +0200

    second-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,25 @@
-export const App = () => {
-  return (
-    <div></div>
-  );
-};
+import React, { Component } from 'react';
+import initialContacts from './data/contacts.json';
+import shortid from 'shortid';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import FormList from './FormList/FormList';
+
+class App extends Component {
+  state = {
+    contacts: initialContacts,
+    filter: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phoneook</h1>
+        <FormList />
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FormList/FormList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Form, Label, Input, Button, Span } from './FormList.styled';
+
+class FormList extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <Form>
+        <Label>
+          <Span>Name</Span>
+          <Input
+            type="text"
+            placeholder="Your name"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          <Span>Number</Span>
+          <Input
+            type="tel"
+            placeholder="Your number"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type='submit'>Add to contacts</Button>
+      </Form>
+    );
+  }
+};
+
+export default FormList;
