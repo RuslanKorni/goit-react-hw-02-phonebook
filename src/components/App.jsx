293ca35f31fe9d0ce20a7212c6c745deb05d4dc1
RuslanKorni <ruslankornijcenko4@gmail.com>
2023-12-15 16:51:26 +0200

    finished-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import initialContacts from './data/contacts.json';
 import shortid from 'shortid';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import FormList from './FormList/FormList';
+import GlobalTitle from './Layout/Title';
+import ContactList from './ContatList/ContactList';
+import Filter from './Filter/Filter';
+
+const notifyOptions = {
+  position: 'bottom-left',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+};
 
 class App extends Component {
   state = {
@@ -11,12 +25,63 @@ class App extends Component {
     filter: '',
   };
 
+  addContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+
+    let isAdded = false;
+    this.state.contacts.forEach(el => {
+      if (el.name.toLowerCase() === normalizedName) {
+        toast.error(`${name}: is already in contacts`, notifyOptions);
+        isAdded = true;
+      }
+    });
+
+    if (isAdded) {
+      return;
+    }
+    const contact = {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  deleteContacts = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
     return (
       <div>
-        <h1>Phoneook</h1>
-        <FormList />
-        <h2>Contacts</h2>
+        <GlobalTitle title="Phoneook" />
+        <FormList onSubmit={this.addContact}/>
+        <GlobalTitle title="Contacts" />
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDelete={this.deleteContacts}
+        />
+        <ToastContainer />
       </div>
     );
   }
--- src/components/FormList/FormList.jsx
+++ src/components/FormList/FormList.jsx
@@ -7,16 +7,39 @@ class FormList extends Component {
     number: '',
   };
 
+  handleChange = name => e => {
+    const { target } = e;
+    this.setState({
+      [name]: target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
   render() {
     const { name, number } = this.state;
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit} autoComplete="off">
         <Label>
           <Span>Name</Span>
           <Input
             type="text"
             placeholder="Your name"
             name="name"
+            value={name}
+            onChange={this.handleChange('name')}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -28,6 +51,8 @@ class FormList extends Component {
             type="tel"
             placeholder="Your number"
             name="number"
+            value={number}
+            onChange={this.handleChange('number')}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
